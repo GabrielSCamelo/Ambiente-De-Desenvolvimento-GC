0a720bb6324e63f1190ceda6f65973fde0fbd062
GabrielSCamelo <101327825+GabrielSCamelo@users.noreply.github.com>
2022-03-24 08:04:04 -0300

    Update Exer9.php

--- php/Exer9.php
+++ php/Exer9.php
@@ -2,14 +2,6 @@
 <html lang="pt-br">
     <head>
         <meta charset="UTF-8">
-        <title>3DAW - Exercício 09 - Matheus Jovem Seixas - 2010478300050</title>
-        <style>
-            body{
-                text-align: center;
-                font-family: Verdana;
-            }
-
-        </style>
     </head>
     <body>
         <?php
